6fe06d06c1415a0ae5498ab71b89e1f37dec1aff
branching <branching@gmail.com>
2019-09-03 17:20:19 +0530

    pavan

---
+++ remote/java/GitTestMaven/src/main/java/com/git/maven/StringHelper.java
@@ -0,0 +1,7 @@
+package com.git.maven;
+public class StringHelper {
+public static void main(String[] args) {
+	System.out.println("pavan kumar ahrahri");
+	System.out.println("this is my pavan branch");
+}
+}
