8854b0ee83aa67304c5ced75b743ea32a62d6c4e
branching <branching@gmail.com>
2019-09-03 15:05:21 +0530

    this my frist

---
+++ remote/GitTest/src/main/java/git/test/mavan/App.java
@@ -0,0 +1,15 @@
+package git.test.mavan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("this is my frist program in git"
+        		+ "");
+    }
+}
